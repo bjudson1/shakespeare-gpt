5760c39d6504718412adba31b212139b74b5e52b
Brenden Judson <bjudson@trek10.com>
2023-04-29 17:10:12 -0400

    add predict

--- shakespeare-gpt/predict.py
+++ shakespeare-gpt/predict.py
@@ -3,6 +3,12 @@ from GPTLanguageModel import GPTLanguageModel
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
 itos = { i:ch for i,ch in enumerate(chars) }
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
